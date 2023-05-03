dc0e2166a5f2f7db016733e698bc797337f69188
Andrii Kaprii <andriikaprii@Andriis-Mac-mini.local>
2023-05-02 17:40:07 -0700

    add navbar and font

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Palette from "./Palette";
 import seedColors from "./seedColors";
 export default class App extends Component {
 	render() {
-		console.log(generatePalette(seedColors[4]));
-
 		return (
 			<>
 				<Palette palette={generatePalette(seedColors[4])} />
--- src/Palette.js
+++ src/Palette.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
-import Slider, { Range } from "rc-slider";
-import "rc-slider/assets/index.css";
-
+import Navbar from "./Navbar";
 import ColorBox from "./ColorBox";
 import "./Palette.css";
 
@@ -20,17 +18,9 @@ export default class Palette extends Component {
 		const colorBoxes = colors[level].map((color) => (
 			<ColorBox background={color.hex} name={color.name} />
 		));
-
 		return (
 			<div className="Palette">
-				<Slider
-					defaultValue={level}
-					min={100}
-					step={100}
-					max={900}
-					onChange={this.changeLevel}
-				/>
-				{/* Navbar goes here */}
+				<Navbar level={level} changeLevel={this.changeLevel} />
 				<div className="Palette-colors">{colorBoxes}</div>
 				{/* Footer */}
 			</div>
