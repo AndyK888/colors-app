d6cbea85f5c2d5d6ed6e3bc9c5c4c3df29c99987
Andrii Kaprii <andriikaprii@Andriis-Mac-mini.local>
2023-05-10 18:13:53 -0700

    start styling MiniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -2,12 +2,44 @@ import React from "react";
 import { createUseStyles } from "react-jss";
 
 const useStyles = createUseStyles({
-	main: {
-		backgroundColor: "purple",
-		border: "1px solid teal",
+	root: {
+		backgroundColor: "white",
+		borderRadius: "5px",
+		border: "1px solid black",
+		padding: "0.5rem",
+		position: "relative",
+		overflow: "hidden",
+		"&:hover": {
+			cursor: "pointer",
+		},
+	},
+	colors: {
+		backgroundColor: "grey",
+	},
+	title: {
+		display: "flex",
+		justifyContent: "space-between",
+		alignItems: "center",
+		margin: "0",
+		color: "black",
+		paddingTop: "0.5rem",
+		fontSize: "1rem",
+		position: "relative",
+	},
+	emoji: {
+		marginLeft: "0.5rem",
+		fontSize: "1.5rem",
 	},
 });
 export default function MiniPalette(props) {
+	const { paletteName, emoji } = props;
 	const classes = useStyles();
-	return <h1 className={classes.main}>MiniPalette</h1>;
+	return (
+		<div className={classes.root}>
+			<div className={classes.colors}></div>
+			<h5 className={classes.title}>
+				{paletteName} <span className={classes.emoji}>{emoji}</span>
+			</h5>
+		</div>
+	);
 }
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -6,14 +6,9 @@ export default class PaletteList extends Component {
 		const { palettes } = this.props;
 		return (
 			<div>
-				<MiniPalette />
 				<h1>React Colors</h1>
 				{palettes.map((palette) => {
-					return (
-						<Link key={palette.id} to={`/palette/${palette.id}`}>
-							{palette.paletteName}
-						</Link>
-					);
+					return <MiniPalette {...palette} />;
 				})}
 			</div>
 		);
